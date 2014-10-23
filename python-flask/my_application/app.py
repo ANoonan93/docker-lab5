c3e9ce829ca9190d60fc1d52cca8d23cf034fdee
Adam <adamnoonan93@gmail.com>
2014-10-23 14:25:56 +0000

    file

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -14,3 +14,11 @@ def upload_file():
     return '',201
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+	return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	return 'Post %d' % post_id
